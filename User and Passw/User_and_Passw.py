a84c2936b36bad6e513ea9cbec75698b282d2a4d
Siil777 <118188578+Siil777@users.noreply.github.com>
2023-01-25 12:26:11 +0200

    Update User_and_Passw.py

--- User and Passw/User_and_Passw.py
+++ User and Passw/User_and_Passw.py
@@ -5,7 +5,7 @@ while True:
     print('would you like to register[1], login[2], exit[0]?')
     u=int(input('action:'))
     if u==0:
-        print('until next time!')
+        print('!good-bye')
         break
     elif u==2:
         login=autoriz(input('login:'),input('password:'))
